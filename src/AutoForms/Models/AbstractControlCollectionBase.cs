7a99d645bad3f051de26e4e67726b6939deed149
Nikita Tribunskiy <nikita.tribunskiy@gmail.com>
2022-07-31 19:57:44 +0300

    Adjust XML documentation

--- src/AutoForms/Models/AbstractControlCollectionBase.cs
+++ src/AutoForms/Models/AbstractControlCollectionBase.cs
@@ -3,14 +3,14 @@
 namespace AutoForms.Models;
 
 /// <summary>
-/// Base type for abstract control that holds another controls.
+/// Base type for controls that holds another ones.
 /// </summary>
 /// <typeparam name="T">The controls collection type</typeparam>
 [ExcludeFromCodeCoverage]
 public abstract class AbstractControlCollectionBase<T> : AbstractControl
 {
     /// <summary>
-    /// Collection of the child controls.
+    /// The child controls.
     /// </summary>
     public T Controls { get; set; } = default!;
 }
